4932f19f1fcafce2e9647fb0c7931e182ce0a5d3
arsha <arshathomasthushara123@gmail.com>
2020-01-16 15:24:40 +0530

    handled network failures

--- app/src/main/java/com/example/filmdetailslistapp/MainActivity.kt
+++ app/src/main/java/com/example/filmdetailslistapp/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.filmdetailslistapp.ui.FilmlistFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
